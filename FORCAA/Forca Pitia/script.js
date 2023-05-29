c2d85f1726b0c16c78e423ef4a6a5fbe7fb7ca90
pitialaab <pitialab.023@gmail.com>
2023-05-28 22:49:02 -0300

    Alteração 2

--- FORCAA/Forca Pitia/script.js
+++ FORCAA/Forca Pitia/script.js
@@ -23,7 +23,7 @@ let count = 0;
 let chosenWord = "";
 
 const displayOptions = () => {
-  optionsContainer.innerHTML += `<h3>Forca Pititinha</h3>`;
+  optionsContainer.innerHTML += `<h3>Forca Pititica</h3>`;
   let buttonCon = document.createElement("div");
   for (let value in options) {
     buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
